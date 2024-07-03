fccb95584b8a0c05621bd058e06f73a7c0bc69df
c.pp <c.pp@navercorp.com>
2024-07-03 19:52:12 +0900

    * 구조 변경

--- Pythagorian Triples/Pythagorian Triples.cpp
+++ Pythagorian Triples/Pythagorian Triples.cpp
@@ -6,20 +6,20 @@
 
 int main() 
 {
-	for (auto const& [x, y, z] : std::views::iota(1)
-        | std::views::transform([](int z) {
-            return std::views::iota(1, z + 1) 
-                | std::views::transform([=](int x) {
-                    return std::views::iota(x, z + 1) 
-                        | std::views::transform([=](int y) {
-                            return std::views::single(std::tuple{ x, y, z }) | std::views::take(x * x + y * y == z * z);
-                        })
-                        | std::views::join;
+    auto pythagorian_triples{
+        std::views::iota(1) | std::views::transform([](int z) {
+            return std::views::iota(1, z + 1) | std::views::transform([=](int x) {
+                return std::views::iota(x, z + 1) | std::views::transform([=](int y) {
+                    return std::views::single(std::tuple{ x, y, z }) | std::views::take(x * x + y * y == z * z); 
                 })
                 | std::views::join;
+            })
+            | std::views::join;
         })
         | std::views::join
-        | std::views::take(20))
+    };
+
+	for (auto const& [x, y, z] : pythagorian_triples | std::views::take(20))
     {
         std::cout << std::format("({},{},{})\n", x, y, z);
     }
